3d4433202ae314887fc5aed7b84212137b93ea7a
Timber Michaud <timichaud@stu.lltc.edu>
2024-09-05 10:07:32 -0500

    final commit

--- index.py
+++ index.py
@@ -2,6 +2,18 @@
 def main():
     #do stuff
     print("Hello World")
+    print()
+    print("My name is:")
+    print("Timber Michaud")
+    print()
+    print("Why am I interested in computer science?")
+    print("I love logistics, math, and puzzles. This has a lot of the same qualities which is why I love CompSci.")
+    print()
+    print("What do I want to do after I graduate from LLTC?")
+    print("I would like to pursue a bachelor's at BSU whether it's immediately or in a year.")
+    print() 
+    print("One fun fact about me is:")
+    print("I'm the oldest girl cousin of 30+ cousins.")
 
 if __name__ == "__main__":
     main()
